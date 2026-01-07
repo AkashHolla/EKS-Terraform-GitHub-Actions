9ce099d60bb8e4ab87c8ce214badb02d85a6bf81
Akash Holla <your-email@example.com>
2026-01-07 11:26:15 +0000

    initial commit

--- modules/iam.tf
+++ modules/iam.tf
@@ -84,10 +84,3 @@ data "tls_certificate" "eks" {
 
 }
 
-resource "aws_iam_openid_connect_provider" "eks_oidc" {
-  url = aws_eks_cluster.eks[0].identity[0].oidc[0].issuer
-
-
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks_certificate.certificates[0].sha1_fingerprint]
-}
